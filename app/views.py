86d11837e26b14cc7f5f8297ee1482230c8a59c6
David Schowalter <david.schowalter@iteratec.com>
2023-11-09 22:19:42 +0100

    Refactors method

--- app/views.py
+++ app/views.py
@@ -3,7 +3,6 @@ import zipfile
 from io import BytesIO
 from pdf2image import convert_from_path
 
-
 import requests
 from flask import Blueprint, render_template, request, redirect, url_for, send_from_directory, flash, make_response, \
     session, send_file
@@ -21,71 +20,75 @@ def appointments():
     if not login_token:
         flash('You need to login first.', 'warning')
         return redirect(url_for('main_bp.login'))
+
     start_date, end_date = get_date_range_from_form()
     calendars = fetch_calendars(login_token)
     selected_calendar_ids = [calendar['id'] for calendar in calendars]
 
-    if request.method == 'POST' and 'fetch_appointments' in request.form:
-        selected_calendar_ids = request.form.getlist('calendar_ids')
-        selected_calendar_ids = [int(id) for id in selected_calendar_ids if id.isdigit()]
-
-        appointments = fetch_appointments(login_token, start_date, end_date, selected_calendar_ids)
-
-        session['fetched_appointments'] = [appointment_to_dict(app) for app in appointments]
-        return render_template('appointments.html', calendars=calendars, selected_calendar_ids=selected_calendar_ids,
-                               appointments=session['fetched_appointments'], start_date=start_date,
-                               end_date=end_date)
-
-    if request.method == 'POST' and 'generate_pdf' in request.form:
-        selected_appointment_ids = request.form.getlist('appointment_id')
-        background_image_stream = None
-        if 'background_image' in request.files:
-            file = request.files['background_image']
-            if file and file.filename != '':
-                background_image_stream = BytesIO(file.read())
-        selected_appointments = [app for app in session.get('fetched_appointments', []) if
-                                 str(app['id']) in selected_appointment_ids]
-        filename = create_pdf(selected_appointments, background_image_stream)
-        return redirect(url_for('main_bp.download_file', filename=filename))
-
     if request.method == 'POST':
-        if 'generate_jpeg' in request.form:
-            # Code to handle PDF creation, conversion to JPEG, and ZIP file creation
+        if 'fetch_appointments' in request.form:
+            appointments = get_and_process_appointments(login_token, start_date, end_date)
+            return render_template('appointments.html', calendars=calendars,
+                                   selected_calendar_ids=selected_calendar_ids,
+                                   appointments=session['fetched_appointments'], start_date=start_date,
+                                   end_date=end_date)
+        elif 'generate_pdf' in request.form:
             selected_appointment_ids = request.form.getlist('appointment_id')
-            background_image_stream = None
-            if 'background_image' in request.files:
-                file = request.files['background_image']
-                if file and file.filename != '':
-                    background_image_stream = BytesIO(file.read())
-            selected_appointments = [app for app in session.get('fetched_appointments', []) if
-                                     str(app['id']) in selected_appointment_ids]
-            pdf_filename = create_pdf(selected_appointments, background_image_stream)
-
-            # Convert the PDF to JPEG files
-            full_pdf_path = os.path.join(Config.FILE_DIRECTORY, pdf_filename)
-
-            images = convert_from_path(full_pdf_path)
-            jpeg_files = []
-            for i, image in enumerate(images):
-                jpeg_stream = BytesIO()
-                image.save(jpeg_stream, 'JPEG')
-                jpeg_stream.seek(0)
-                jpeg_files.append((f'page_{i + 1}.jpg', jpeg_stream))
-
-            # Create a ZIP file in memory
-            zip_buffer = BytesIO()
-            with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as zip_file:
-                for file_name, file_bytes in jpeg_files:
-                    zip_file.writestr(file_name, file_bytes.read())
-            zip_buffer.seek(0)
-
-            # Send the ZIP file to the client
+            pdf_filename = handle_pdf_generation(selected_appointment_ids)
+            return redirect(url_for('main_bp.download_file', filename=pdf_filename))
+        elif 'generate_jpeg' in request.form:
+            selected_appointment_ids = request.form.getlist('appointment_id')
+            pdf_filename = handle_pdf_generation(selected_appointment_ids)
+            zip_buffer = handle_jpeg_generation(pdf_filename)
             return send_file(zip_buffer, mimetype='application/zip', as_attachment=True, download_name='images.zip')
 
-    # This else clause is for when there's no GET or POST, which shouldn't normally happen
-    else:
-        return render_template('appointments.html', calendars=calendars, selected_calendar_ids=selected_calendar_ids,
-                               start_date=start_date, end_date=end_date)
+    return render_template('appointments.html', calendars=calendars, selected_calendar_ids=selected_calendar_ids,
+                           start_date=start_date, end_date=end_date)
+
+
+def get_and_process_appointments(login_token, start_date, end_date):
+    selected_calendar_ids = request.form.getlist('calendar_ids')
+    selected_calendar_ids = [int(id) for id in selected_calendar_ids if id.isdigit()]
+
+    appointments = fetch_appointments(login_token, start_date, end_date, selected_calendar_ids)
+    session['fetched_appointments'] = [appointment_to_dict(app) for app in appointments]
+    return appointments
+
+
+def handle_pdf_generation(appointment_ids):
+    background_image_stream = get_background_image_stream()
+    selected_appointments = [app for app in session.get('fetched_appointments', []) if
+                             str(app['id']) in appointment_ids]
+    filename = create_pdf(selected_appointments, background_image_stream)
+    return filename
+
+
+def handle_jpeg_generation(pdf_filename):
+    full_pdf_path = os.path.join(Config.FILE_DIRECTORY, pdf_filename)
+    images = convert_from_path(full_pdf_path)
+    jpeg_files = []
+
+    for i, image in enumerate(images):
+        jpeg_stream = BytesIO()
+        image.save(jpeg_stream, 'JPEG')
+        jpeg_stream.seek(0)
+        jpeg_files.append((f'page_{i + 1}.jpg', jpeg_stream))
+
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as zip_file:
+        for file_name, file_bytes in jpeg_files:
+            zip_file.writestr(file_name, file_bytes.read())
+    zip_buffer.seek(0)
+    return zip_buffer
+
+
+def get_background_image_stream():
+    background_image_stream = None
+    if 'background_image' in request.files:
+        file = request.files['background_image']
+        if file and file.filename != '':
+            background_image_stream = BytesIO(file.read())
+    return background_image_stream
 
 
 @main_bp.route('/', methods=['GET', 'POST'])
