bb5f1a8ad62f8ea1b0f512f42dfa66ea8bca0997
David Schowalter <david.schowalter@iteratec.com>
2023-11-09 16:55:21 +0100

    More dynamic rectangle height

--- app/pdf_generator.py
+++ app/pdf_generator.py
@@ -37,8 +37,8 @@ def draw_background_image(canvas, image_stream, page_width, page_height):
 
 
 # Define the 16:9 page size in points
-PAGE_WIDTH = 800
-PAGE_HEIGHT = 450
+PAGE_WIDTH = 1200
+PAGE_HEIGHT = 675
 PAGE_SIZE = (PAGE_WIDTH, PAGE_HEIGHT)
 
 
@@ -83,22 +83,46 @@ def create_pdf(appointments, image_stream=None):
         date_key = start_dt.strftime('%d.%m.%Y')
         appointments_by_date[date_key].append(a)
 
-    left_column_x = 100
-    right_column_x = 400
-    y_position = landscape(PAGE_SIZE)[1] - 100
     indent = 15
     bottom_margin = 50  # Bottom margin before adding a new page
 
+    # Calculate relative positions based on page size
+    left_margin_ratio = 1/30  # example: 1/30th of the page width
+    right_column_ratio = 2/5  # position the right column at 2/5 of the page width
+
+    left_column_x = PAGE_WIDTH * left_margin_ratio
+    right_column_x = PAGE_WIDTH * right_column_ratio
+    y_position = PAGE_HEIGHT - (PAGE_HEIGHT * 1/10)  # 1/10th from the top of the page
+
+    # Calculate the height and width of the rectangle relative to page size
+    rect_width = PAGE_WIDTH - (2 * left_column_x)  # width minus double the left margin
+
+    # Define font sizes relative to page height
+    base_font_size = PAGE_HEIGHT / 35  # Base font size is set relative to page height
+    font_size_large = base_font_size * 1.5  # Large font for headers
+    font_size_medium = base_font_size * 1.2  # Medium font for subheaders
+    font_size_small = base_font_size  # Small font for details
+    line_spacing = font_size_medium * 1.5  # Dynamic line spacing based on font size
+
     for date_key, events in sorted(appointments_by_date.items()):
         for event in events:
-            rect_height = 100
-            rect_width = landscape(PAGE_SIZE)[0] - 200
+
+            # Calculate the total text block height for each appointment
+            total_text_height = 0
+            total_text_height += font_size_large + line_spacing  # For the German Day and Date
+            total_text_height += font_size_medium + line_spacing  # For the Time and MeetingAt
+            details_count = len(event['information'].split('\n'))
+            total_text_height += (font_size_small) * details_count  # For the details with adjusted line spacing
+
+            # Now set the rectangle height to match the total text height
+            rect_height = total_text_height + line_spacing  # Add some padding
 
             # Check if we need to start a new page
             if y_position < (rect_height + bottom_margin):
                 c.showPage()
                 c.setPageSize(landscape(PAGE_SIZE))
-                y_position = landscape(PAGE_SIZE)[1] - 100
+                y_position = PAGE_HEIGHT - (PAGE_HEIGHT * 1/10)
+
                 if image_stream:
                     draw_background_image(c, image_stream, *landscape(PAGE_SIZE))
 
@@ -106,38 +130,40 @@ def create_pdf(appointments, image_stream=None):
 
             # Left column: German Day and Date
             c.setFillColor(HexColor(0xC1540C))
-            c.setFont("Helvetica-Bold", 18)
+            c.setFont("Helvetica-Bold", font_size_large)
             german_day_of_week = format_date(start_dt, format='EEEE', locale='de_DE')
             day_date_str = f"{german_day_of_week}, {date_key}"
-            c.drawString(left_column_x + indent, y_position - 25, day_date_str)  # German Day and Date
+            c.drawString(left_column_x + indent, y_position - line_spacing, day_date_str)  # German Day and Date
 
-            darkgrey_color = HexColor(0x4E4E4E)
             # Time and MeetingAt
-            c.setFillColor(darkgrey_color)
-            c.setFont("Helvetica", 12)
+            c.setFillColor(HexColor(0x4E4E4E))
+            c.setFont("Helvetica", font_size_medium)
             time_str = f"{start_dt.strftime('%H:%M')} Uhr"
-            time_width = c.stringWidth(time_str, "Helvetica", 12)
-            c.drawString(left_column_x + indent, y_position - 45, time_str)  # Time
+            time_width = c.stringWidth(time_str, "Helvetica", font_size_medium)
+            c.drawString(left_column_x + indent, y_position - (2 * line_spacing), time_str)  # Time
 
             if event['meetingAt']:
                 meeting_at_str = f"{event['meetingAt']}"
-                c.drawString(left_column_x + indent + time_width + 10, y_position - 45,
+                c.drawString(left_column_x + indent + time_width + 10, y_position - (2 * line_spacing),
                              meeting_at_str)  # MeetingAt, adjust spacing as needed
 
             # Right column: Caption and Information
             c.setFillColor(black)
-            c.setFont("Helvetica-Bold", 18)
-            c.drawString(right_column_x, y_position - 25, event['description'])  # Caption
+            c.setFont("Helvetica-Bold", font_size_large)
+            c.drawString(right_column_x, y_position - line_spacing, event['description'])  # Caption
 
-            c.setFillColor(darkgrey_color)
-            c.setFont("Helvetica", 10)
-            details_y_position = y_position - 45
+            c.setFillColor(HexColor(0x4E4E4E))
+            c.setFont("Helvetica", font_size_small)
+            details_y_position = y_position - (2 * line_spacing)  # Start below the caption
             for detail in event['information'].split('\n'):
                 c.drawString(right_column_x, details_y_position, detail)
-                details_y_position -= 15  # Adjust line spacing
+                details_y_position -= font_size_small * 1.5  # Adjust line spacing based on font size
+
+
 
             # Update y_position for next event
-            y_position -= (rect_height + 20)
+            y_position -= (rect_height + line_spacing)  # space between rectangles
 
     c.save()
     return filename
+
